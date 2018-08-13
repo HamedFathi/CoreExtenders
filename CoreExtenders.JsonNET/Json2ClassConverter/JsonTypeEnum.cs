1f155c82cf32ccf89bb23b5722e1a67c73835812
Hamed Fathi <noeffect@outlook.com>
2018-08-13 15:19:46 +0430

    add(Json2ClassConverter)

---
+++ CoreExtenders.JsonNET/Json2ClassConverter/JsonTypeEnum.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2010 Xamasoft
+
+
+namespace CoreExtenders
+{
+    public enum JsonTypeEnum
+    {
+        Anything,
+        String,
+        Boolean,
+        Integer,
+        Long,
+        Float,
+        Date,
+        NullableInteger,
+        NullableLong,
+        NullableFloat,
+        NullableBoolean,
+        NullableDate,
+        Object,
+        Array,
+        Dictionary,
+        NullableSomething,
+        NonConstrained
+
+
+    }
+}
